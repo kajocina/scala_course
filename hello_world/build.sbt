095ec696a6fe6d9fd1e0abf801ad0e3e51283071
kajocina <kajocina@gmail.com>
2017-06-29 15:14:01 +0200

    first push, exercises

---
+++ hello_world/build.sbt
@@ -0,0 +1,6 @@
+name := "hello_world"
+
+version := "1.0"
+
+scalaVersion := "2.12.1"
+        
---
+++ hello_world/src/main/scala/hellow.scala
@@ -0,0 +1,8 @@
+/**
+  * Created by kaj on 6/28/17.
+  */
+package example
+
+object hellow extends App {
+  println("Hello World")
+}
